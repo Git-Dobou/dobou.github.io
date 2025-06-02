5d279c8daa1fe5cc3b4d86a5c2998a847dcfb0f3
Dobou <DobouD@verband.creditreform.de>
2025-06-02 14:29:54 +0200

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "08ed8bb98922271daa13f9ac4c05aef9",
+"flutter_bootstrap.js": "b943248433ad3ae7c74904417a0a799f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7e28f2ed565a964af00246129fd4a3c2",
-"/": "7e28f2ed565a964af00246129fd4a3c2",
-"main.dart.js": "440aba5a5bd48d9e4ca44103e5cdf4f2",
+"index.html": "23ffc27fa573427c6868aa3b8e1792f9",
+"/": "23ffc27fa573427c6868aa3b8e1792f9",
+"main.dart.js": "671acc2230717fb273fab9e063ae8d7e",
 "manifest.json": "cb1efbbeeddbe3476b91d68544daa34d",
 "version.json": "1f2dcea2a632335292e092b396c67e5c"};
 // The application shell files that are downloaded before a service worker can
