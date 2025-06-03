a69d137b5d2f5f7cf49a2791809022e1a4f53f0e
Dobou <DobouD@verband.creditreform.de>
2025-06-03 21:35:29 +0200

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3200344637"
+    serviceWorkerVersion: "667956353"
   }
 });
